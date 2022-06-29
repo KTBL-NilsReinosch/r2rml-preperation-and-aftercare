2ed599f4fbd7b020ef6a77d0aff24ad6fda365d2
KTBL-NilsReinosch <78076264+KTBL-NilsReinosch@users.noreply.github.com>
2022-06-29 15:18:37 +0200

    Saves now as UTF-8 without BOM
    Saves now as UTF-8 without BOM, also removed useless header option

--- csv_prep.py
+++ csv_prep.py
@@ -11,6 +11,13 @@
 # Also all predicates that are selected from thoses will start with lowerchase letter.
 # Also there are some costume replacements in the headers that simply can be put in at costumeReplace and costumeReplacements.
 
+# Error?
+# UnicodeDecodeError: 'utf-8' codec can't decode byte 0x9f in position 12: invalid start byte
+# try saaving the csv as UFT-8 with BOM and then again as UFT-8
+
+# UFT-8 geht nicht?
+# Sicher das es als CSV UFT-8 abgespeichert wurde?
+
 
 import csv
 
@@ -54,22 +61,18 @@ while coliumnPredicate != procede:
 print('Secelted Columns:')
 print(uriPred)
 
-# If the costume replacements shoulde be active or not
-while ((useHeader != 1) and (useHeader != 0)):
-    print("Do you wish to enable the costume replacements (need to be changed in the programm)?")
-    useHeader = int(input("1 = yes or 0 = no: "))
-print(useHeader)
 
 
 # Which columns need to be turned into suitable URIs?
 #uriRow = [0, 2, 3, 4]
 #uriPred = [2, 3]
-#useHeader = True
 
+
+# py csv_prep.py
 
 # All general signs that need to be removed from a URI
-replaceText = (' ', ',', ':', '/', '?', '#', '[', ']', '@', '!', '$', '%', '&', '\'', '(', ')', '*', '+', ';', '=', '-', '.', '_', '~')
-replaceWith = ('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '')
+replaceText = [' ', ',', ':', '/', '?', '#', '[', ']', '@', '!', '$', '%', '&', '\'', '(', ')', '*', '+', ';', '=', '-', '.', '_', '~']
+replaceWith = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
 
 
 # For predicates replacing the first capital letter with lowercase ones
@@ -85,14 +88,14 @@ capitalLettersSpace = [" " + x for x in capitalLetters]
 lowercaseLettersSpace = [" " + v for v in lowercaseLetters]
 
 # Costume Replacements in Header
-costumeReplace = ( 'Datentyp', 'Nutzung', 'GgfsAlternativeGebräuchlichEinheiten', 'Überüberbegriff', 'Überbegriff', 'Einheit', 'BezeichnungDerEigenschaft')
-costumeReplacements = ('Dat entyp', 'Nut zung', 'XXX AE', 'XUE UE', 'XXX UE', 'XXX E', 'Bez eichnungDerEigenschaft')
+costumeReplace = ['Datentyp', 'Nutzung', 'GgfsAlternativeGebräuchlichEinheiten', 'Überüberbegriff', 'Überbegriff', 'Einheit', 'BezeichnungDerEigenschaft']
+costumeReplacements = ['Dat entyp', 'Nut zung', 'XXX AE', 'XUE UE', 'XXX UE', 'XXX E', 'Bez eichnungDerEigenschaft']
 
 
 
 
-# Opens the csv file with reading permition in UTF-8
-with open(fileName +'.csv', 'r', newline='', encoding='utf-8') as csv_file:
+# Opens the csv file with reading permition in UTF-8 without BOM
+with open(fileName +'.csv', 'r', newline='', encoding='utf-8-sig') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=";")
 
 
@@ -108,6 +111,10 @@ with open(fileName +'.csv', 'r', newline='', encoding='utf-8') as csv_file:
             for i in uriRow:
                 r = j[i]
 
+                # Removing special charakters form URIS the URIs CamelCase
+                #for q in range(len(specialCharakters)):
+                #    r = r.replace(specialCharakters[q], specialCharaktersReplacemnet[q])
+
                 # Making the URIs CamelCase
                 for e in range(len(lowercaseLettersSpace)):
                     r = r.replace(lowercaseLettersSpace[e], capitalLettersSpace[e])
@@ -115,11 +122,6 @@ with open(fileName +'.csv', 'r', newline='', encoding='utf-8') as csv_file:
                 # Removes all the text pieces you do not want in a URI
                 for c in range(len(replaceText)):
                     r = r.replace(replaceText[c], replaceWith[c])
-
-                # Get the costume replacements done if Header was set to 1
-                if ((b == 1) and (useHeader == 1)):
-                    for a in range(len(costumeReplace)):
-                        r = r.replace(costumeReplace[a], costumeReplacements[a])
 
                 # If the column is ment to be a predicate the first letter will be replaced with a lowercase letter of itself
                 if ((i in uriPred) and (r)):
